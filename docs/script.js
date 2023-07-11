913520fbbca3660e736f9f7fe92184751a978e90
dbeley <6568955+dbeley@users.noreply.github.com>
2023-07-11 12:11:11 +0200

    Update page title

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	//Only needed for the filename of export files.
 	//Normally set in the title tag of your page.
-	document.title='my-steam-library';
+	document.title='My Steam Library';
   	var numbersType = $.fn.dataTable.absoluteOrderNumber( [
     	{ value: 'N/A', position: 'bottom' }
   	] );
