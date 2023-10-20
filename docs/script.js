1612f734b355d9f1488e79a4a3019a3074de8704
dbeley <6568955+dbeley@users.noreply.github.com>
2023-10-21 00:15:59 +0200

    Add custom ordering for achievements column

--- docs/script.js
+++ docs/script.js
@@ -16,6 +16,62 @@ $(document).ready(function() {
             return data;
             }
     };
+    $.extend( $.fn.dataTable.ext.type.order, {
+      "achievements-custom-sort-desc": function (val_1, val_2) {
+        var regex = /(\d+(\.\d+)?)/g;
+        var match1 = val_1.match(regex);
+        var match2 = val_2.match(regex);
+
+        if (match1 && match2) {
+          var percentage1 = parseInt(match1[match1.length - 1]);
+          var total1 = parseInt(match1[match1.length - 2]);
+          var percentage2 = parseInt(match2[match2.length - 1]);
+          var total2 = parseInt(match2[match2.length - 2]);
+
+          if (percentage1 > percentage2) {
+            return 1;
+          } else if (percentage1 < percentage2) {
+            return -1;
+          }
+          if (total1 > total2) {
+            return 1;
+          } else if (total1 < total2) {
+            return -1;
+          }
+        }
+
+        return 0;
+      }
+    } );
+
+
+    $.extend( $.fn.dataTable.ext.type.order, {
+      "achievements-custom-sort-asc": function (val_1, val_2) {
+        var regex = /(\d+(\.\d+)?)/g;
+        var match1 = val_1.match(regex);
+        var match2 = val_2.match(regex);
+
+        if (match1 && match2) {
+          var percentage1 = parseInt(match1[match1.length - 1]);
+          var total1 = parseInt(match1[match1.length - 2]);
+          var percentage2 = parseInt(match2[match2.length - 1]);
+          var total2 = parseInt(match2[match2.length - 2]);
+
+          if (percentage1 < percentage2) {
+            return 1;
+          } else if (percentage1 > percentage2) {
+            return -1;
+          }
+          if (total1 < total2) {
+            return 1;
+          } else if (total1 > total2) {
+            return -1;
+          }
+        }
+
+        return 0;
+      }
+    } );
 
     // DataTable initialisation
     $('#insights').DataTable(
@@ -35,6 +91,7 @@ $(document).ready(function() {
             "scrollToTop": true,
             "order": [[6, 'desc']],
             "columnDefs": [
+                { type: "achievements-custom-sort", targets: [9] },
                 { type: numbersType, targets: [1] },
                 { targets: [2], render: DataTable.render.number() },
                 { targets: [4, 6], render: DataTable.render.formatDuration() },
